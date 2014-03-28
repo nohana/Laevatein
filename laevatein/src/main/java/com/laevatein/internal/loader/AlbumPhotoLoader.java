5317d169216efb4cea4508d11231f352663c388e
KeithYokoma <Keith.Yokoma@gmail.com>
2014-03-28 14:51:41 +0900

    Disable camera capture on no camera feature device

--- laevatein/src/main/java/com/laevatein/internal/loader/AlbumPhotoLoader.java
+++ laevatein/src/main/java/com/laevatein/internal/loader/AlbumPhotoLoader.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2014 nohana, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an &quot;AS IS&quot; BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.laevatein.internal.loader;
 
 import com.laevatein.internal.entity.Album;
@@ -11,9 +26,13 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v4.content.CursorLoader;
 
+import jp.mixi.compatibility.android.provider.MediaStoreCompat;
+
 /**
- * @author keishin.yokomaku
+ * @author KeithYokoma
  * @since 2014/03/27
+ * @version 1.0.0
+ * @hide
  */
 public class AlbumPhotoLoader extends CursorLoader {
     public static final String TAG = AlbumPhotoLoader.class.getSimpleName();
@@ -41,7 +60,7 @@ public class AlbumPhotoLoader extends CursorLoader {
     @Override
     public Cursor loadInBackground() {
         Cursor result = super.loadInBackground();
-        if (!mEnableCapture) {
+        if (!mEnableCapture || !MediaStoreCompat.hasCameraFeature(getContext())) {
             return result;
         }
         MatrixCursor dummy = new MatrixCursor(PROJECTION);
