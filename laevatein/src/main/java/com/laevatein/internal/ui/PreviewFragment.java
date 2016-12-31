1c63a087b8ff2a47fa138cbc3d44637860a06871
hiroyuki.seto <hiroyuki.seto@nohana.co.jp>
2016-12-31 16:58:07 +0900

    refactor layout resources

--- laevatein/src/main/java/com/laevatein/internal/ui/PreviewFragment.java
+++ laevatein/src/main/java/com/laevatein/internal/ui/PreviewFragment.java
@@ -23,34 +23,30 @@ import it.sephiroth.android.library.imagezoom.ImageViewTouchBase;
  */
 public class PreviewFragment extends Fragment {
     private static final String ARGS_URI = BundleUtils.buildKey(PhotoGridFragment.class, "ARGS_URI");
-    private static final String ARGS_LAYOUT_ID = BundleUtils.buildKey(PhotoGridFragment.class, "ARGS_LAYOUT_ID");
-    private static final String ARGS_IMAGE_VIEW_ID = BundleUtils.buildKey(PhotoGridFragment.class, "ARGS_IMAGE_VIEW_ID");
+    private static final String ARGS_RESOURCES = BundleUtils.buildKey(PhotoGridFragment.class, "ARGS_RESOURCES");
 
-    private int mLayoutId;
-    private int mImageViewId;
+    private PreviewViewResources mViewResources;
 
     public static PreviewFragment newInstance(Uri uri, PreviewViewResources resources) {
         PreviewFragment fragment = new PreviewFragment();
         Bundle bundle = new Bundle();
         bundle.putParcelable(ARGS_URI, uri);
-        bundle.putInt(ARGS_LAYOUT_ID, resources.getLayoutId());
-        bundle.putInt(ARGS_IMAGE_VIEW_ID, resources.getImageViewId());
+        bundle.putParcelable(ARGS_RESOURCES, resources);
         fragment.setArguments(bundle);
         return fragment;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        mLayoutId = getArguments().getInt(ARGS_LAYOUT_ID);
-        mImageViewId = getArguments().getInt(ARGS_IMAGE_VIEW_ID);
-        return inflater.inflate(mLayoutId, container, false);
+        mViewResources = getArguments().getParcelable(ARGS_RESOURCES);
+        return inflater.inflate(mViewResources.getLayoutId(), container, false);
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        ImageViewTouch image = (ImageViewTouch) getView().findViewById(mImageViewId);
+        ImageViewTouch image = (ImageViewTouch) getView().findViewById(mViewResources.getImageViewId());
         image.setDisplayType(ImageViewTouchBase.DisplayType.FIT_TO_SCREEN);
         Uri uri = getArguments().getParcelable(ARGS_URI);
         Point size = PhotoMetadataUtils.getBitmapSize(getActivity().getContentResolver(), uri, getActivity());
