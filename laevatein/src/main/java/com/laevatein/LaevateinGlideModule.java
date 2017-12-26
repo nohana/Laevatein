ce961026914a2723655b1dea53231dd46ffecb2e
koichi.kukino <koichi.kukino.ts@nohana.co.jp>
2017-12-26 18:44:03 +0900

    Hello Glide. Goodbye Picasso.

---
+++ laevatein/src/main/java/com/laevatein/LaevateinGlideModule.java
@@ -0,0 +1,8 @@
+package com.laevatein;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public final class LaevateinGlideModule extends AppGlideModule {
+}
